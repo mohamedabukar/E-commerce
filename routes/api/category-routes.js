72227b0eaf6adb765f2967d9139b23805b20adc2
Mohamed <mohamed.h.abukar@gmail.com>
2021-08-16 18:54:18 -0500

    get api route for find all categories created.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,24 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]
+    }
+  })
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
